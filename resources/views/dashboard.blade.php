eb77595bf40a8b1a2a55a90c3489ca91c2a97142
Virgiawan Listiyandi <listiyandiv@gmail.com>
2023-03-22 15:47:31 +0700

    feat: admin template

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@push('scripts')
+    <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
+@endpush
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="content-footer footer bg-footer-theme py-3">
+    <div class="container-fluid d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            ©
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            , made with ❤️ by
+            <a href="" class="footer-link fw-bolder">Virgiawan Listiyandi</a>
+        </div>
+    </div>
+</footer>
